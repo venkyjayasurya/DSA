fd92980f1068541b7651e01606bc36bcf4889d5a
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-07-09 23:25:39 +0530

    Optimized

--- src/Algorithms/Sorting/BubbleSort.java
+++ src/Algorithms/Sorting/BubbleSort.java
@@ -15,9 +15,9 @@ import java.util.Scanner;
 public class BubbleSort {
 
     public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n-1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+        int n = arr.length - 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
