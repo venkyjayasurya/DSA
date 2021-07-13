5e4d6c0d9c8a5958dc08d0f68c0f24982e6c3223
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-07-14 00:20:52 +0530

    Documenting Insertion Sort

---
+++ src/Algorithms/Sorting/InsertionSort.java
@@ -0,0 +1,16 @@
+package Algorithms.Sorting;
+
+/*
+* Here we have two sublists in an array -> Sorted sublist and Unsorted sublist
+* We will be inserting unsorted sublist elements into sorted sublist one by one
+*
+* First Array[0] will be considered as the first element of sorted sublist
+* And remaining all values i.e, Array[0] to Array[n-1] will be unsorted sublist elments
+*
+*
+*
+* */
+
+
+public class InsertionSort {
+}
