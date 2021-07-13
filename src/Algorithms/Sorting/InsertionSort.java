51707b08cc5d6eaff150226f1a14e1ea6076ed54
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-07-14 01:38:04 +0530

    Debugging Insertion Sort

--- src/Algorithms/Sorting/InsertionSort.java
+++ src/Algorithms/Sorting/InsertionSort.java
@@ -7,7 +7,8 @@ package Algorithms.Sorting;
 * First Array[0] will be considered as the first element of sorted sublist
 * And remaining all values i.e, Array[0] to Array[n-1] will be unsorted sublist elments
 *
-*
+* Worst Case Time Complexity : O(n^2)  => If the array is unsorted
+* Best Case Time Complexity  : O(n)    => if the array is sorted
 *
 * */
 
@@ -20,19 +21,25 @@ public class InsertionSort {
             int temp = arr[i];              //Second element in the array / First element in the unsorted sublist
             int j = i-1;                    //j value is the sorted sublist last element
             while(j>=0 && arr[j] > temp){   //Loop until sorted element is greater than temp value
+
                 arr[j+1] = arr[j];          // Swap the elements if the previous element is greater than current
                 j--;
+                printArray(arr);
+                System.out.printf(" i = %d, j = %d, temp = %d", i,j,temp);
             }
-            arr[j+1] = temp;                //
+            arr[j+1] = temp;
+            printArray(arr);
         }
     }
 
     public static void printArray(int[] arr) {
         for (int i : arr) System.out.print(i + " ");
+        System.out.println();
     }
 
     public static void main(String[] args){
         int[] arr = {5, 4, 10, 1 ,6, 2};
+        printArray(arr);
         Sort(arr);
         printArray(arr);
     }
