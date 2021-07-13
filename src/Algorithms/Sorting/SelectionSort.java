7febbd502c38781f464410eaf1eda6e37461cfa1
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-07-14 02:30:15 +0530

    Modify selection sort

--- src/Algorithms/Sorting/SelectionSort.java
+++ src/Algorithms/Sorting/SelectionSort.java
@@ -4,26 +4,33 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 class SelectionSort {
-    public static void selectionSort(int[] array) {
-        int size = array.length;
-
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for(int i=0; i< n-1; i++){
+            int min = i;            //Initially min is a[0]
+            for(int j=i+1; j<n; j++){
+                if(arr[j] < arr[min]){
+                    min = j;
+                }
+            }
+            if(min != i){       //Swap the values arr[i] , arr[min]
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+        }
     }
 
     public static void main(String[] args) {
-//        int[] data = { 20, 12, 10, 15, 2 };
-
         Scanner in = new Scanner(System.in);
-
         System.out.println("enter number of elements");
         int size = in.nextInt();
-
         int[] data = new int[size];
-
         System.out.println("enter elements");
         for (int i = 0; i < size; i++) {
             data[i] = in.nextInt();
         }
-
+//        int[] data = { 20, 12, 10, 15, 2 };
         selectionSort(data);
         System.out.println("Sorted Array in Ascending Order: ");
         System.out.println(Arrays.toString(data));
