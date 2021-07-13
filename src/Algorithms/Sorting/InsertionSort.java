f4b03a564174c735e3593f46e400c46b55906b3f
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-07-14 01:21:38 +0530

    Documenting Insertion Sort

--- src/Algorithms/Sorting/InsertionSort.java
+++ src/Algorithms/Sorting/InsertionSort.java
@@ -13,4 +13,27 @@ package Algorithms.Sorting;
 
 
 public class InsertionSort {
+
+    public static void Sort(int[] arr){
+        int n = arr.length;
+        for(int i=1; i<n;i++){
+            int temp = arr[i];              //Second element in the array / First element in the unsorted sublist
+            int j = i-1;                    //j value is the sorted sublist last element
+            while(j>=0 && arr[j] > temp){   //Loop until sorted element is greater than temp value
+                arr[j+1] = arr[j];          // Swap the elements if the previous element is greater than current
+                j--;
+            }
+            arr[j+1] = temp;                //
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i : arr) System.out.print(i + " ");
+    }
+
+    public static void main(String[] args){
+        int[] arr = {5, 4, 10, 1 ,6, 2};
+        Sort(arr);
+        printArray(arr);
+    }
 }
