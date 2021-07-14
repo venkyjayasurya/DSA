5d9c42fa533249fd5fc4d143006fb5b84190fd81
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-07-15 02:19:19 +0530

    Learning Quick Sort..

---
+++ src/Algorithms/Sorting/QuickSort.java
@@ -0,0 +1,20 @@
+package Algorithms.Sorting;
+
+/*
+*
+* Quick Sort is based on divide and conquer technique
+* The Array is divided into sub arrays
+*
+* Let the first element is the pivot alement
+* The elements less than the pivot element goes to left side of the pivot
+* and similarly the values greater than pivot element goes to right side of the pivot
+*                                 key
+*            {  Partion 1 }    |  Pivot  |   {  Partion 2  }
+*           values < pivot                    values > pivot
+* */
+
+public class QuickSort {
+    public static void main(String[] args){
+
+    }
+}
