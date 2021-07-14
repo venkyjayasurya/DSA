08a89ffdc30eef55af71cd4495dbf6ce30ade6c7
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-07-15 03:15:54 +0530

    Quick Sort Algorithm with Documentation

--- src/Algorithms/Sorting/QuickSort.java
+++ src/Algorithms/Sorting/QuickSort.java
@@ -12,7 +12,6 @@ package Algorithms.Sorting;
 *            {  Partion 1 }    |  Pivot  |   {  Partion 2  }
 *           values < pivot                    values > pivot
 *
-*
 * Time Complexity:
 *       Worst Case              -> O(n^2)
 *       Best or Average Case    -> O( n log n )
@@ -20,39 +19,39 @@ package Algorithms.Sorting;
 
 public class QuickSort {
 
-    public static int partition(int[] arr, int lowerBound, int upperBound){
-        int pivot = arr[lowerBound];
-        int start = lowerBound;
-        int end = upperBound;
-
-        while(start < end){
-            while(arr[start] <= pivot){
-                start++;
-            }
-
-            while(arr[end] > pivot){
-                end--;
-            }
-
-            if(start < end){
-//            swap(arr[start], arr[end]);
-                int temp = arr[start];
-                arr[start] = arr[end];
-                arr[end] = temp;
+    public static int partition(int[] arr, int lowerBound, int upperBound)
+    {
+        int pivot = arr[upperBound];
+        int i = (lowerBound-1); // index of smaller element
+        for (int j=lowerBound; j<upperBound; j++)
+        {
+            // If current element is smaller than or
+            // equal to pivot
+            if (arr[j] <= pivot)
+            {
+                i++;
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
 
-//        swap(arr[lowerBound] , arr[end]
-        int temp = arr[lowerBound];
-        arr[lowerBound] = arr[end];
-        arr[end] = temp;
-        return end;
+        // swap arr[i+1] and arr[upperBound] (or pivot)
+        int temp = arr[i+1];
+        arr[i+1] = arr[upperBound];
+        arr[upperBound] = temp;
+
+        return i+1;
     }
 
-    public static void Sort(int[] arr, int lowerBound, int upperBound){
-        if(lowerBound < upperBound){
-            int loc = partition(arr, lowerBound, upperBound);
-            Sort(arr, lowerBound, loc-1);
+    public static void Sort(int[] arr, int lowerBounderBound, int upperBound){
+        if(lowerBounderBound < upperBound){
+            // find pivot element location such that
+            // elements smaller than pivot are on the left
+            // elements greater than pivot are on the right
+            int loc = partition(arr, lowerBounderBound, upperBound);
+            Sort(arr, lowerBounderBound, loc-1);
             Sort(arr, loc+1, upperBound);
         }
     }
@@ -65,7 +64,7 @@ public class QuickSort {
     public static void main(String[] args){
         int[] arr = {7, 6, 10, 5, 9, 2, 1, 15, 7};
         printArray(arr);
-        Sort(arr, 0, arr.length-1);
+        Sort(arr, 0, (arr.length) - 1);
         System.out.println("The Sorted Array is");
         printArray(arr);
     }
