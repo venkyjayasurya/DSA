6119f2caa6d5da9cef46b6c1a0dae834eaede72e
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-07-20 00:29:10 +0530

    Learning Radix Sort...

--- src/Algorithms/Sorting/RadixSort.java
+++ src/Algorithms/Sorting/RadixSort.java
@@ -2,8 +2,12 @@ package Algorithms.Sorting;
 
 /*
 * Radix Sort is applied digit by digit or on place values of the array element
-* 
+* {678, 111, 234, 980, 142, 4}
+*  1 9 9
+*  2 1 0   <- Start from Least Significant bit
 *
+* Find max data first say 980
+* and make all the numbers to the same size say make all the elements of 3 digits size
 * */
 
 public class RadixSort {
