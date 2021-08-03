e6b18c8670f9d07162ba4d9823c78b47f8fe59fe
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-08-03 15:02:33 +0530

    Pvr Anniversary - virtusa

---
+++ src/Virtusa/PvrAnniversary.java
@@ -0,0 +1,51 @@
+package Virtusa;
+
+/*
+* 5
+* elite or premium -> p/e
+* refreshments/not -> y/n
+* special coupons -> y/n
+*
+* */
+
+import java.util.Scanner;
+
+public class PvrAnniversary {
+    public int TotalTicketPrice(int NoOfTickets){
+        int cost=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Are you interested in Elite or Premium");
+        String eOrP = sc.nextLine();
+        if(eOrP.equals("e")){
+//            Cost = 75
+            cost = NoOfTickets * 75;
+        }else{
+            cost = NoOfTickets * 120;
+        }
+        cost -=  (cost/100 * 12);
+        System.out.println("Are you interested Refreshments or not");
+        String refresh = sc.nextLine();
+        if(refresh.equals("y")){
+            cost += 50;
+        }
+        System.out.println("Do you have any special coupons");
+        String coupons = sc.nextLine();
+        if(coupons.equals('y')){
+            cost -= (cost/100 * 8);
+        }
+        return cost;
+    }
+
+    public static void main(String[] args){
+        System.out.println("PVR Anniversary");
+        Scanner sc= new Scanner(System.in);
+        int NoOfTickets = sc.nextInt();
+        if(NoOfTickets>4 && NoOfTickets<40){
+            int TotalCost = PvrAnniversary(NoOfTickets);
+            System.out.println("Total Cost = " + TotalCost);
+        }else{
+            System.out.println("Thanks for the request, You cannot book tickets now!");
+        }
+
+    }
+}
