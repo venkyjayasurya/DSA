3060378df71740caad0cb66af225cf5e9e4d9acb
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-08-03 15:18:35 +0530

    Pvr Anniversary - virtusa

--- src/Virtusa/PvrAnniversary.java
+++ src/Virtusa/PvrAnniversary.java
@@ -1,18 +1,10 @@
 package Virtusa;
 
-/*
-* 5
-* elite or premium -> p/e
-* refreshments/not -> y/n
-* special coupons -> y/n
-*
-* */
-
 import java.util.Scanner;
 
 public class PvrAnniversary {
-    public static int TotalTicketPrice(int NoOfTickets){
-        int cost=0;
+    int TotalTicketPrice(int NoOfTickets){
+        int cost=0, refreshCost=0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Are you interested in Elite or Premium");
         String eOrP = sc.nextLine();
@@ -26,22 +18,24 @@ public class PvrAnniversary {
         System.out.println("Are you interested Refreshments or not");
         String refresh = sc.nextLine();
         if(refresh.equals("y")){
-            cost = cost + 50;
+            int refreshCost = (50 * NoOfTickets);
         }
         System.out.println("Do you have any special coupons");
         String coupons = sc.nextLine();
         if(coupons.equals("y")){
             cost = cost - (cost/100 * 8);
         }
+        cost = cost + refreshCost;
         return cost;
     }
 
     public static void main(String[] args){
+        PvrAnniversary obj= new PvrAnniversary();
         System.out.println("PVR Anniversary");
         Scanner sc= new Scanner(System.in);
         int NoOfTickets = sc.nextInt();
         if(NoOfTickets>4 && NoOfTickets<40){
-            int TotalCost = TotalTicketPrice(NoOfTickets);
+            int TotalCost = obj.TotalTicketPrice(NoOfTickets);
             System.out.println("Total Cost = " + TotalCost);
         }else{
             System.out.println("Thanks for the request, You cannot book tickets now!");
